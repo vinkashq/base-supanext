52be5deed6a765440540fe8a55c27a14918fbd21
Vinoth Kannan <svkn.87@gmail.com>
2026-05-02 23:19:11 +0530

    feat: implement header and footer components with navigation menu layout

---
+++ components/footer/index.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-muted py-4">
+      <div className="container-wrapper px-6 group-has-data-[slot=designer]/layout:max-w-none 3xl:fixed:px-0">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col md:flex-row gap-2 items-center justify-between text-xs text-muted-foreground">
+
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ components/header/index.tsx
@@ -0,0 +1,26 @@
+import { CurrentUserAvatar } from "../current-user-avatar"
+import { Button } from "../ui/button"
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "../ui/navigation-menu"
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <header className="w-full bg-background">
+      <div className="container-wrapper px-6 group-has-data-[slot=designer]/layout:max-w-none 3xl:fixed:px-0">
+        <div className="flex flex-col lg:flex-row lg:justify-between h-(--header-height) items-center **:data-[slot=separator]:h-4! group-has-data-[slot=designer]/layout:fixed:max-w-none 3xl:fixed:container">
+          <Button render={<Link className="flex items-center gap-2" href="/" />} nativeButton={false} variant="ghost" className="h-12 my-1">
+            Vinkas Base
+          </Button>
+          <NavigationMenu className="font-semibold">
+            <NavigationMenuList>
+              <NavigationMenuItem className="hidden md:list-item">
+                <NavigationMenuLink href="/">Home</NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <CurrentUserAvatar />
+        </div>
+      </div>
+    </header>
+  )
+}
