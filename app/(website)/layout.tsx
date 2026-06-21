93c94a75441fa77827d6a4540f576ebaa5953276
Vinoth Kannan <svkn.87@gmail.com>
2026-06-21 13:37:01 +0530

    refactor: reorganize project structure by moving website components into a dedicated (website) route group

---
+++ app/(website)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
