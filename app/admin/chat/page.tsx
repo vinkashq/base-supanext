b699453c4ee5cc40b282c6af812935e24e28e195
Vinoth Kannan <svkn.87@gmail.com>
2026-07-07 20:19:02 +0530

    feat: implement chat interface with message input, collapsible sidebar menu, and new UI components

--- app/admin/chat/page.tsx
+++ app/admin/chat/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { Bubble, BubbleContent } from "@/components/ui/bubble"
 import { Button } from "@/components/ui/button"
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
+import { InputGroup, InputGroupAddon, InputGroupButton } from "@/components/ui/input-group"
 import { Message, MessageContent } from "@/components/ui/message"
 import {
   MessageScroller,
@@ -11,37 +15,41 @@ import {
   MessageScrollerProvider,
   MessageScrollerViewport,
 } from "@/components/ui/message-scroller"
+import { Textarea } from "@/components/ui/textarea"
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
+import { ArrowUpIcon, GlobeIcon, ImageIcon, MessageCircleDashedIcon, PaperclipIcon, PlusIcon, RotateCcwIcon, TelescopeIcon } from "lucide-react"
 
 export default function Page() {
   const messages = [{ id: "1", role: "user", content: "Hello" }, { id: "2", role: "ai", content: "Hello 2" }]
+  const nextMessage = null
+  const isBusy = false
   return (
     <MessageScrollerProvider autoScroll>
-      <div className="relative flex flex-col gap-4">
-        <Card className="mx-auto h-140 w-full max-w-sm gap-0">
+      <div className="relative flex flex-col gap-4 flex-1">
+        <Card className="mx-auto h-140 w-full gap-0 rounded-t-none flex-1">
           <CardHeader className="gap-1 border-b">
             <CardTitle>New Chat</CardTitle>
             <CardDescription>How can I help you today?</CardDescription>
             <CardAction>
               <Tooltip>
-                <TooltipTrigger render={<Button variant="outline" size="icon" aria-label="Reset conversation">Reset</Button>} />
+                <TooltipTrigger render={<Button variant="outline" size="icon" aria-label="Reset conversation"><RotateCcwIcon /></Button>} />
                 <TooltipContent>
-                  <p>Reset</p>
+                  Reset conversation
                 </TooltipContent>
               </Tooltip>
             </CardAction>
           </CardHeader>
-          <CardContent className="flex-1 overflow-hidden p-0">
+          <CardContent className="flex-1 overflow-hidden">
             {messages.length === 0 ? (
               <Empty className="h-full">
                 <EmptyHeader>
                   <EmptyMedia variant="icon">
-                    Icon
+                    <MessageCircleDashedIcon />
                   </EmptyMedia>
-                  <EmptyTitle>Morning, shadcn!</EmptyTitle>
+                  <EmptyTitle>Good day!</EmptyTitle>
                   <EmptyDescription>
-                    What are we working on today? Press send to start a new
-                    conversation
+                    What can I help you with today?
                   </EmptyDescription>
                 </EmptyHeader>
               </Empty>
@@ -49,17 +57,18 @@ export default function Page() {
               <MessageScroller>
                 <MessageScrollerViewport>
                   <MessageScrollerContent>
-                    {messages.map((message) => (
+                    {messages.map((message, index) => (
                       <MessageScrollerItem
                         key={message.id}
                         messageId={message.id}
                         scrollAnchor={message.role === "user"}
+                        className={cn("", index === 0 && "mt-4")}
                       >
                         <Message>
                           <MessageContent>
                             <Bubble
                               align={message.role === "user" ? "end" : "start"}
-                              variant={message.role === "user" ? "default" : "muted"}
+                              variant={message.role === "user" ? "muted" : "ghost"}
                             >
                               <BubbleContent>{message.content}</BubbleContent>
                             </Bubble>
@@ -73,6 +82,61 @@ export default function Page() {
               </MessageScroller>
             )}
           </CardContent>
+          <CardFooter className="flex-col gap-2 p-0 dark:bg-background">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                if (!nextMessage || isBusy) {
+                  return
+                }
+                // void sendMessage(nextMessage)
+              }}
+              className="w-full"
+            >
+              <InputGroup className="border-0 bg-transparent dark:bg-transparent">
+                <Textarea autoFocus placeholder="Type your message here..." className="h-24 w-full px-3 py-2.5 bg-transparent dark:bg-transparent resize-none border-0 focus-visible:ring-0">
+                  {nextMessage ? nextMessage : ""}
+                </Textarea>
+                <InputGroupAddon align="block-end" className="pt-1">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger render={<InputGroupButton aria-label="Add files" type="button" size="icon-sm" variant="outline"><PlusIcon /></InputGroupButton>} />
+                    <DropdownMenuContent
+                      align="start"
+                      side="top"
+                      className="w-44"
+                    >
+                      <DropdownMenuItem>
+                        <PaperclipIcon />
+                        Add Photos & Files
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <ImageIcon />
+                        Create Image
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <TelescopeIcon />
+                        Deep Research
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <GlobeIcon />
+                        Web Search
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  <InputGroupButton
+                    type="submit"
+                    variant="default"
+                    size="icon-sm"
+                    className="ml-auto"
+                  >
+                    <ArrowUpIcon />
+                    <span className="sr-only">Send</span>
+                  </InputGroupButton>
+                </InputGroupAddon>
+              </InputGroup>
+            </form>
+          </CardFooter>
         </Card>
       </div>
     </MessageScrollerProvider>
