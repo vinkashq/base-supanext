2b78e029f55f3182ffb5e03447e4edcda72a8483
Vinoth Kannan <svkn.87@gmail.com>
2026-07-12 12:12:24 +0530

    feat: implement account dashboard structure with authenticated profile page and sidebar navigation

---
+++ app/account/page.tsx
@@ -0,0 +1,20 @@
+import { LogoutButton } from '@/components/logout-button'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function Page() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getClaims()
+  if (error || !data?.claims) {
+    throw new Error("Unauthorized")
+  }
+
+  return (
+    <div className="flex h-svh w-full items-center justify-center gap-2">
+      <p>
+        Hello <span>{data.claims.email}</span>
+      </p>
+      <LogoutButton />
+    </div>
+  )
+}
